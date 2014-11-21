e311479f523e7a7664129d00775a45b56f09bdba
mrubart <mrubart@gmail.com>
2014-11-21 12:54:05 -0700

    oh yeah!

---
+++ AngularApp/Scripts/AngularApp.js
@@ -0,0 +1,70 @@
+﻿var AngularApp = angular.module('AngularApp', ['ui.router','ui.bootstrap']);
+
+AngularApp.controller('LandingPageController', LandingPageController);
+AngularApp.controller('LoginController', LoginController);
+AngularApp.controller('RegisterController', RegisterController);
+
+AngularApp.factory('AuthHttpResponseInterceptor', AuthHttpResponseInterceptor);
+AngularApp.factory('LoginFactory', LoginFactory);
+AngularApp.factory('RegistrationFactory', RegistrationFactory);
+
+var configFunction = function($stateProvider, $httpProvider, $locationProvider) {
+
+	$locationProvider.hashPrefix('!').html5Mode(true);
+
+	$stateProvider
+		.state('stateOne', {
+			url: '/stateOne?donuts',
+			views: {
+				"containerOne": {
+					templateUrl: '/routesDemo/one'
+				},
+				"containerTwo": {
+					templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts; }
+				},
+				"nestedView@stateOne": {
+					templateUrl: '/routesDemo/four'
+				}
+			}
+		})
+		.state('stateTwo', {
+			url: '/stateTwo',
+			views: {
+				"containerOne": {
+					templateUrl: '/routesDemo/one'
+				},
+				"containerTwo": {
+					templateUrl: 'routesDemo/three'
+				}
+			}
+		})
+		.state('stateThree', {
+			url: '/stateThree?donuts',
+			views: {
+				"containerOne": {
+					templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts; }
+				},
+				"containerTwo": {
+					templateUrl: 'routesDemo/three'
+				}
+			}
+		})
+		.state('loginRegister', {
+			url: 'loginRegister?returnUrl',
+			views: {
+				"containerOne": {
+					templateUrl: '/Account/Login',
+					controller: LoginController
+				},
+				"containerTwo": {
+					templateUrl: '/Account/Register',
+					controller: RegisterController
+				}
+			}
+		});
+	$httpProvider.interceptors.push('AuthHttpResponseInterceptor');
+};
+
+configFunction.$inject = ['$stateProvider', '$httpProvider', '$locationProvider'];
+
+AngularApp.config(configFunction);
---
+++ AngularApp/Scripts/Factories/RegistrationFactory.js
@@ -0,0 +1,26 @@
+﻿var RegistrationFactory = function($http, $q) {
+	return function(emailAddress, password, confirmPassword) {
+		var deferredObject = $q.defer();
+		$http.post(
+			'/Account/Register', {
+				Email: emailAddress,
+				Password: password,
+				ConfirmPassword: confirmPassword
+			})
+			.success(function(data) {
+				if (data === true) {
+					deferredObject.resolve({ success: true });
+				} else {
+					deferredObject.resolve({ success: false });
+				}
+
+			})
+			.error(function () {
+				deferredObject.resolve({ success: false });
+			});
+
+		return deferredObject.promise;
+	};
+};
+
+RegistrationFactory.$inject = ['$http', '$q'];
